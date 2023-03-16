7f7c15d1f2f3f12c3aca26de71fd60db32709e55
fagwes <109398136+fagwes@users.noreply.github.com>
2023-03-16 12:50:48 -0300

    Update untitled0.py

--- untitled0.py
+++ untitled0.py
@@ -7,5 +7,24 @@ Original file is located at
     https://colab.research.google.com/drive/1_GbMQHufAn_8tLI7vJCUHjhAR81U4DOK
 """
 
-nome = 'Wesley'
-print('Bem vindo,', nome,)
+# String nome e Imprimir
+
+nome='Wesley'
+print('Seja bem vindo',nome,)
+
+
+# Indentação correta (01)|Indentação Incorreta (02)
+# código 01
+x = 4
+if x > 5:
+ x = x * 2
+ print(x)
+print('fim')
+
+# código 02
+x = 4
+if x > 5:
+ x = x * 2
+print(x)
+print('fim')
+
