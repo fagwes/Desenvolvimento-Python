e76f117ed9841a83e6cc92571f0e21ea755872e0
fagwes <109398136+fagwes@users.noreply.github.com>
2023-04-11 20:58:00 -0300

    Update Meus Testes4.py

--- Meus Testes4.py
+++ Meus Testes4.py
@@ -3,5 +3,7 @@ idades = [27, 49, 12, 67, 21, 32, 18, 45, 84, 53, 22, 56, 80, 35, 18]
 print('Maior idade:', max(idades))
 print('Menor idade:', min(idades))
 
-Resultado: Maior idade: 84
+Resultado: 
+Maior idade: 84
+Menor idade: 12
 
