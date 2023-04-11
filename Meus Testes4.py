9692a116154a79953d1d9a0c15fc965dafa2551b
fagwes <109398136+fagwes@users.noreply.github.com>
2023-04-11 20:57:10 -0300

    Update Meus Testes4.py

--- Meus Testes4.py
+++ Meus Testes4.py
@@ -1,7 +1,7 @@
-# Verifica Maior idade
-
+# Verifica Maior e Menor idade
 idades = [27, 49, 12, 67, 21, 32, 18, 45, 84, 53, 22, 56, 80, 35, 18]
 print('Maior idade:', max(idades))
+print('Menor idade:', min(idades))
 
 Resultado: Maior idade: 84
 
